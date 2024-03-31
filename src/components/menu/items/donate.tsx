d11daf69d5ae2cdcea0612a1bf739fbb6664f745
Younes Bensitel <younes.bensitel@hotmail.com>
2024-04-01 03:34:54 +0800

    resolve icons

--- src/components/menu/items/donate.tsx
+++ src/components/menu/items/donate.tsx
@@ -5,9 +5,9 @@ import { Item } from '../item';
 export function Donate() {
   return (
     <Item
-      href="https://buymeacoffee.com/remvze"
-      icon={<SiBuymeacoffee />}
-      label="Buy Me a Coffee"
+      href="https://buy.stripe.com/bIY3fpb2w0Gdeys3cc"
+      icon={<BiDonateHeart />}
+      label="Donate ❤️"
     />
   );
 }
